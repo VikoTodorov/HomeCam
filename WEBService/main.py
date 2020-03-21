34634dc144c03a78ee5d6929dd42a767177d19a8
KonstantinDonev <donev.konstantin@gmail.com>
2020-03-21 20:30:41 +0000

    added login and register

--- WEBService/main.py
+++ WEBService/main.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import *
 import createdb
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
