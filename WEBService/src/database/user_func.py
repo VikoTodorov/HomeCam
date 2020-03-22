1c76d4358f242bfbb7d3e428d54f069176e84d56
Viko <vikotodorov02@gmail.com>
2020-03-22 19:41:04 +0200

    fixing insert in database

--- WEBService/src/database/user_func.py
+++ WEBService/src/database/user_func.py
@@ -5,6 +5,7 @@ def insert_user(values):
     with database.connect() as conn:
         conn.cursor().execute('''INSERT INTO Users(Fname, Lname, Email, Psw)
                               VALUES(?, ?, ?, ?);''', values)
+        conn.cursor().commit()
 
 
 def exctract_user(email):
