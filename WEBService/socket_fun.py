1788a2e224c58b5c235998db0f22ecd71d7959d2
viko <vikotodorov02@gmail.com>
2020-05-31 20:43:59 +0300

    removing one while

--- WEBService/socket_fun.py
+++ WEBService/socket_fun.py
@@ -52,17 +52,23 @@ def getFrame(sock, conn):
 def decrypt(conn):
     keys_size = struct.calcsize('LL')
     data = b''
-    while True:
-        while len(data) < keys_size:
-            data += conn.recv(4096)
-        info = data[:keys_size]
-        data = data[keys_size:]
-        frame_size = struct.unpack('LL', info)[0]
-        key_size = struct.unpack('LL', info)[1]
-        while len(data) < frame_size+key_size:
-            data += conn.recv(4096)
-        frame_data = data[:frame_size]
-        key_data = data[frame_size:]
-        frame = pickle.loads(frame_data)
-        key = pickle.loads(key_data)
-        return frame, key
+
+    while len(data) < keys_size:
+        data += conn.recv(4096)
+
+    info = data[:keys_size]
+    data = data[keys_size:]
+
+    frame_size = struct.unpack('LL', info)[0]
+    key_size = struct.unpack('LL', info)[1]
+
+    while len(data) < frame_size+key_size:
+        data += conn.recv(4096)
+
+    frame_data = data[:frame_size]
+    key_data = data[frame_size:]
+
+    frame = pickle.loads(frame_data)
+    key = pickle.loads(key_data)
+
+    return frame, key
